0add5fd836170ef7cdd005c52010ee6ec7e8107b
Isaac-MCastanho <fgcastanho@hotmail.com>
2023-03-23 19:03:04 -0300

    Estrutura e configurações iniciais do projeto

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const routers = require("./router");
+
+const app = express();
+
+app.use(routers);
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,7 @@
+const app = require("./app");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 3333;
 
+app.listen(PORT, () => {
+	console.log(`Server running or port ${PORT}`);
+});

b08502c1bf988eb1cae31b1b06e2bbd1f22bad4f
Isaac-MCastanho <fgcastanho@hotmail.com>
2023-03-25 00:11:38 -0300

    middleware validateTasks OK

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 const routers = require("express").Router();
 const tasksController = require("./controllers/tasksController");
+const tasksMiddlewares = require("./middlewares/tasksMiddleware");
 
 routers.get("/tasks", tasksController.getAll);
-routers.post("/tasks", tasksController.post);
+routers.post("/tasks", tasksMiddlewares.validateBody, tasksController.post);
 
 module.exports = routers;
